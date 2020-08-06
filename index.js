fac69db61d9aa06118a1d3807ecc905e84add7d5
Jonathan <jonathanthornton1997@gmail.com>
2020-08-06 10:28:08 -0700

    added .env

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+require('dotenv').config() // if there is a .env on the root folder it will merge it into process.env
+
 const express = require("express");
 const server = express();
 
 const port = process.env.PORT || 5000;
 
 server.get("/", (req, res) => {
-  res.status(200).json("Hello from the new server");
+    const message = process.env.MESSAGE || 'Hello from the new server'
+  res.status(200).json({message});
 });
 
 server.listen(port, () => {
